2f2a892544dd5e9da9cc55f50617a573d5126703
Ozias Dushimimana <odushimi@gmail.com>
2019-11-11 18:09:24 +0300

    firebase, sign with google

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import FormInput from './../../components/form-input/form-input.component';
 import CustomButton from './../../components/custom-button/custom-button.component';
+import { signInWithGoogle } from './../../firebase/firebase.utils';
+
 
 import './sign-in.styles.scss';
 ;
@@ -52,8 +54,10 @@ class SignIn extends React.Component {
                         label='Password'
                         required />
 
-                    <CustomButton type='submit'>Sign in</CustomButton>
-
+                    <div className='buttons'>
+                        <CustomButton type='submit'>Sign in</CustomButton>
+                        <CustomButton onClick={ signInWithGoogle } isGoogleSignIn > Sign in with Google </CustomButton>
+                    </div>
                 </form>
             </div>
         );
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,34 @@
+import firebase from 'firebase/app';
+
+import 'firebase/firestore';
+import 'firebase/auth';
+
+
+const config = {
+    apiKey: "AIzaSyBPpHljG05YtUS3dCVmo-jRVBWi1xhqRH0",
+    authDomain: "od-clothing.firebaseapp.com",
+    databaseURL: "https://od-clothing.firebaseio.com",
+    projectId: "od-clothing",
+    storageBucket: "od-clothing.appspot.com",
+    messagingSenderId: "341139105037",
+    appId: "1:341139105037:web:e19a89ff2494fc9ecf3c44",
+    measurementId: "G-98G4J1GYZG"
+  };
+
+
+  firebase.initializeApp(config);
+
+
+  export const auth = firebase.auth();
+  export const firestore = firebase.firestore();
+
+
+  // Setup google auth utility
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({ prompt: 'select_account'});
+  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+  export default firebase;
+  
+
+
