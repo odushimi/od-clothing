0330263a6d4d80cfd0543ad41921de791723a562
Ozias Dushimimana <odushimi@gmail.com>
2019-11-11 17:08:36 +0300

    sign in form

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component.jsx';
+
 
 function App() {
   return (
@@ -15,6 +17,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInSignUpPage} />
       </Switch>
     </div>
   );
