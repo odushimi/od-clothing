b78870bd2d00af37eb46cb2b06bde60435227526
Ozias Dushimimana <odushimi@gmail.com>
2020-01-02 15:41:20 +0300

    added checkout-item

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Checkout from './pages/checkout/checkout.component';
+
+
 import Header from './components/header/header.component';
+
+
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 
@@ -46,6 +51,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout'  component={Checkout} />
           <Route exact path='/signin' render = {() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)}  />
         </Switch>
       </div>
